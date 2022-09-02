23dd3c00f1952d9fa46c01639c12564d404b5385
WQhaunm <2942096269@qq.com>
2022-09-02 13:37:06 +0800

    Add files via upload

---
+++ 3，arr数组/Project3/arr数组.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+
+                 //数组是用下标来访问，从0开始，如前10个数就是0-9
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 }; //定义arr为10个数              
+	int a = 0;
+	while (a < 10)
+	{
+		printf("%d\n", arr[a]);
+		a++;
+	}
+
+
+
+
+	return 0;
+}
+
+
+
+
+//使用scanf，arr是数组，不用&arr，只需要arr          scanf("%d",arr)
+//但是arr[a]不是数组，所以要&      scanf("%d",&arr[a])
