6c6c5565d498174849eb3b8d78feb9623395378e
WQhaunm <2942096269@qq.com>
2022-09-02 13:40:45 +0800

    Add files via upload

---
+++ 8，switch语句/Project8/switch语句.c
@@ -0,0 +1,122 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+//
+//       //打出星期
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);    //scanf不要在%d后面手残写出\n
+//	switch (day)    //day不需要“”来引用
+//	{
+//	case 1:     //case为案列，指switch中具体示例，要记得加:      而且case只能用整型常量表达式，不可以有浮点数
+//		printf("星期1\n");
+//		break;     //break表示跳出，若没加break，则会从输入位置走到最后再return
+//	case 2:
+//		printf("星期2\n");
+//		break;
+//	case 3:
+//		printf("星期3\n");
+//		break;
+//	case 4:
+//		printf("星期4\n");
+//		break;
+//	case 5:
+//		printf("星期5\n");
+//		break;
+//	case 6:
+//		printf("星期6\n");
+//		break;
+//	case 7:
+//		printf("星期7\n");
+//		break;
+//
+//	}
+//
+//	return 0;
+//	}
+
+
+//
+////打出工作日，休息日
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("休息日");
+//		break;
+//	default:              //default表示其余（错误）输入
+//		printf("暂无此选项");
+//
+//
+//	}
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
