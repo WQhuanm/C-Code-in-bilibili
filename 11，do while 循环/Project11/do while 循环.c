e24e9c89b8188ea610e20cd553831fbded966534
WQhaunm <2942096269@qq.com>
2022-09-02 13:51:48 +0800

    Add files via upload

---
+++ 11，do while 循环/Project11/do while 循环.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+//
+//         //do while循环是先执行后判断
+//int main()
+//{
+//	int i = 0;
+//	do {
+//		if (5 == i)
+//		{
+//			break;
+//		}
+//			printf("%d ", i);
+//		
+//			i++;
+//	} while (i < 10);
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
