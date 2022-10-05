a258e6bb798cc6d99c3b6b50632fb1b8c35b5a49
WQhuanm <2942096269@qq.com>
2022-10-05 15:20:44 +0800

    Add files via upload

---
+++ 字符串拷贝strcpy/字符串拷贝strcpy/源.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+#include<algorithm>
+
+using namespace std;
+
+
+
+////strcpy头文件是include<string.h>
+//int main() {
+//    char a[10] = "sdoafofoa";
+//    char b[10] = "hello";
+//    strcpy(a, b);//将a替换为b
+//    printf("%s", a);
+//    return 0;
+//}
+//
+
+
