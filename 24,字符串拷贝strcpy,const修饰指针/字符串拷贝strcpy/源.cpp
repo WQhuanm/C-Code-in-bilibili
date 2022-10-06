9ce9e1e355e359058092c724290be6ac3ff81337
WQhuanm <2942096269@qq.com>
2022-10-06 22:53:20 +0800

    Add files via upload

---
+++ 24,字符串拷贝strcpy,const修饰指针/字符串拷贝strcpy/源.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+#include<algorithm>
+
+using namespace std;
+
+
+
+////strcpy头文件是include<string.h>
+//int main() {
+//    char a[10] = "sdoafofoa";
+//    char b[10] = "hello";
+//    strcpy(a, b);//将a替换为b
+//    printf("%s", a);
+//    return 0;
+//}
+//
+
+  
+
+
+          //const 修饰指针
+   //int const*p=&n,那么const修饰*p，则*p不可以改变（指针指向的内容不可以改变），但是p可以改为=&a，&b等等
+//int *const p=&n,那么p不可以改变（指针变量不可以变），但是指针指向的内容能变，但是p只能是&n,不能其他
+
+
+
